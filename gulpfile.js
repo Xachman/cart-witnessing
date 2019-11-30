cf2648246539294bc05d5d3dcee1a99de4951f42
xachman <zironside@hotmail.com>
2019-11-30 12:49:25 +0000

    update node and npm deps

--- gulpfile.js
+++ gulpfile.js
@@ -2,11 +2,11 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 
 gulp.task('styles', function() {
-    gulp.src('sass/**/*.scss')
+    return gulp.src('sass/**/*.scss')
         .pipe(sass().on('error', sass.logError))
         .pipe(gulp.dest('./webroot/css/'));
 });
 
 gulp.task('watch',function() {
-    gulp.watch('sass/**/*.scss',['styles']);
+    return gulp.watch('sass/**/*.scss', gulp.series('styles'));
 });
